551ae6d829af2b833ad42f3773199e79795071d1
RSU TJS STEM <technologyjusticestudies@rsu.edu>
2026-09-25 12:16:33 -0500

    Change emojis in title and info messages
    Updated title and messages with new emojis.

--- app_start_commented.py
+++ app_start_commented.py
@@ -17,11 +17,11 @@ import streamlit as st
 # - Writing your own welcome message
 
 
-st.title("My Emoji Translator 💬➡️😎")
+st.title("My Emoji Translator 💬➡️😁")
 
-st.write("Turn your words into emojis! 🚀")
+st.write("Turn your words into emojis! ♻️")
 
-st.info("💡 Try typing: I love my cat")
+st.info("💡 Try typing: I'm scared of flying")
 
 
 # ============================================================
@@ -83,6 +83,10 @@ EMOJI_DICT = {
     "dead": "🥀",
     "tired": "💤‍🫩",
     "see": "👀",
+    "pout" : "🥺"
+    "think" : "🤔"
+    "flying" : "✈️"
+    
 }
 
 
